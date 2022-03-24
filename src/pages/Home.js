0068cd944a0f3ede7f81a6dadd4ad45b3d853a31
Olamide <soluade101@gmail.com>
2022-03-24 22:03:26 +0800

    cleanups

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,11 +3,7 @@ import theme from '../data/theme';
 import Header from '../components/Header';
 import Hero from '../components/Hero';
 import Footer from '../components/Footer';
-// import Cta from '../components/Cta';
 import Download from '../components/Download';
-import FAQ from '../components/FAQ';
-import Features from '../components/Features';
-import RegistrationModal from '../components/RegistrationModal';
 import Sponsors from '../components/Sponsors';
 
 const Home = () => {
@@ -18,8 +14,6 @@ const Home = () => {
             <Hero theme={theme} />
             <Download theme={theme} />
             <Sponsors />
-            {/* <FAQ theme={theme} />
-            <Cta theme={theme} />*/}
             <Footer theme={theme} />
         </>
     )
