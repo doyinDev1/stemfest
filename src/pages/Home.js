58fc3df0b30c3906fa731913b4b3c20064de861e
Daramola femi <dfmopeoluwa@gmail.com>
2022-03-26 10:57:45 +0100

    Header component created

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,21 @@
+import Header from "../components/Header";
 
+import homeImg from "../images/assets/Typing.JPG";
 
 const Home = () => {
+  return (
+    <div className="home">
+      <Header
+        header="Welcome to our school"
+        title="A brighter Future <h2>for your kids</h2>"
+        image={homeImg}
+        about="
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut porro corporis mollitia aliquid? Corrupti, laboriosam."
+        link="/login"
+        linkText="Button"
+      />
+    </div>
+  );
+};
 
-    return (
-        <>
-          Home 
-        </>
-    )
-}
-
-export default Home
+export default Home;
