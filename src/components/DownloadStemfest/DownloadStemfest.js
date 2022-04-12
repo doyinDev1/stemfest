fcec5e7ef50fcee4975450ecaac4ed39a4d2b580
Abisola-Morohunfolu <amorohunfolu@gmail.com>
2022-04-12 16:20:44 +0100

    webpack fix

--- src/components/DownloadStemfest/DownloadStemfest.js
+++ src/components/DownloadStemfest/DownloadStemfest.js
@@ -1,38 +1,32 @@
 import { saveAs } from 'file-saver';
-// import { pdf } from '@react-pdf/renderer';
+import { pdf } from '@react-pdf/renderer';
 import { CloudDownloadOutlined } from '@mui/icons-material';
 import { Button, Spinner } from 'react-bootstrap';
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import classes from './DownloadStemfest.module.css';
 import PDFStemfest from '../PDFStemfest/PDFStemfest';
 
-const DownloadStemfest = ({ status}) => {
+const DownloadStemfest = ({ status }) => {
 	const [downloading, setDownloading] = useState(false);
 	// const userInfo = JSON.parse(sessionStorage.getItem('rpUser'));
-	const currentDate = new Date();
+	// const currentDate = new Date();
 
-	const userInfo = sessionStorage.getItem('cpUser')
-
-
+	// const userInfo = sessionStorage.getItem('cpUser');
 
 	// console.log(ImageLink);
 
-	// const generatePDFDoc = async () => {
-	// 	setDownloading(true);
-	// 	// const blob = await pdf(<PDFCertificate nameData={data.userdata?.name} CertificateDate={CertificateDate} />).toBlob();
-	// 	const blob = await pdf(
-	// 		<PDFStemfest
-				
-	// 		/>
-	// 	).toBlob();
-	// 	saveAs(blob, `Stemfest Badge.pdf`); // `${data.userdata?.name}Stemfest Badge.pdf`);
-	// 	setDownloading(false);
-	// };
+	const generatePDFDoc = async () => {
+		setDownloading(true);
+		// const blob = await pdf(<PDFCertificate nameData={data.userdata?.name} CertificateDate={CertificateDate} />).toBlob();
+		const blob = await pdf(<PDFStemfest />).toBlob();
+		saveAs(blob, `Stemfest Badge.pdf`); // `${data.userdata?.name}Stemfest Badge.pdf`);
+		setDownloading(false);
+	};
 
 	return (
 		<Button
 			disabled={!(status === true)}
-			// onClick={generatePDFDoc}
+			onClick={generatePDFDoc}
 			className={classes.DownloadBtn}
 			variant="outlined-success"
 		>
